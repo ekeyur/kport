fe8c0e22e9b2bf03d1a885de68e8d830807ced5e
Keyur Patel <ekeyur@gmail.com>
2017-02-28 15:49:10 -0500

    remaking the portfolio

---
+++ js/portfolio.js
@@ -0,0 +1,38 @@
+var app = angular.module('portfolio', []);
+app.controller('portfolioController', function($scope, $timeout, $interval) {
+$scope.homeshow = false;
+$scope.aboutshow = true;
+$scope.portfolioshow = true;
+$scope.letschatshow = true;
+
+$scope.aboutClicked = function(){
+  $scope.homeshow = true;
+  $scope.aboutshow = false;
+  $scope.portfolioshow = true;
+  $scope.letschatshow = true;
+};
+
+$scope.homeClicked = function(){
+  $scope.homeshow = false;
+  $scope.aboutshow = true;
+  $scope.portfolioshow = true;
+  $scope.letschatshow = true;
+};
+
+$scope.portfolioClicked = function(){
+  $scope.homeshow = true;
+  $scope.aboutshow = true;
+  $scope.portfolioshow = false;
+  $scope.letschatshow = true;
+};
+
+$scope.letschateClicked = function(){
+  $scope.homeshow = true;
+  $scope.aboutshow = true;
+  $scope.portfolioshow = true;
+  $scope.letschatshow = false;
+};
+
+
+
+});
